3b430916ca1faa1d4a0352587de92246648d9607
unknown <jlumley0413@gmail.com>
2022-07-21 19:37:44 -0400

    Updated Attack moves and character classes to mesh better. Probably still not totally functional

--- Arena_Class.py
+++ Arena_Class.py
@@ -1,23 +1,61 @@
-from character.py import *
+from character import *
 import time
 import random
 import os
-from Attack_moves.py import *
+#from Attack_moves import *
+import json
+
+#NEED TO TURN THIS INTO A CLASS, SO CHARACTERS CAN FIGHT MULTIPLE TIMES
+
+#Importing json
+with open("character_class_configs.json") as f:
+    reference_class_dict = json.load(f)
+    
+with open('item_config.json') as data_file:
+    item_data = json.load(data_file)
+
 
 #Here is where the characters are generated
 
 char1_name = input("Player 1: What do you want your character's name to be?  ")
 char1_type = input("Player 1: What do you want your character's type to be?  ")
+char1_item = input("Player 1: What do you want your character's item to be?  ")
 
 char2_name = input("Player 2: What do you want your character's name to be?  ")
 char2_type = input("Player 2: What do you want your character's type to be?  ")
+char2_item = input("Player 2: What do you want your character's item to be?  ")
 
-char1 = character(char1_type, char1_name)
-char2 = character(char2_type, char2_name)
+char1 = char(reference_class_dict, char1_type, char1_name)
+char2 = char(reference_class_dict, char2_type, char2_name)
+
+#NEED TO ADD ITEMS
+
+#Combat happens
+while char1.alive and char2.alive:
+    print("Initiating Next Round of Combat!")
+    #char1_attack = int(input("Character 1, which attack do you want to use? Enter p for physical or m for magic: "))
+    #os.system('clear')
+    #char2_attack = int(input("Character 1, which attack do you want to use? Enter p for physical or m for magic: "))
+    #os.system('clear')
+    char1_damage_dealt = char1.calculate_attack() - char2.calculate_defense()
+    char2_damage_dealt = char2.calculate_attack() - char1.calculate_defense()
+
+    char1.take_damage(char2_damage_dealt)
+    char2.take_damage(char1_damage_dealt)
+
+    print("Char 1 health remaining: ", char1.current_health)
+    print("Char 2 health remaining: ", char2.current_health)
+
+#End of Combat
+if not char1.alive:
+    print("Character 1 has been defeated. Congratulations, character 2!")
+if not char2.alive:
+    print("Character 2 has been defeated. Congratulations, character 1!")
 
 
 
-    #Print information for each player here
+
+     #Print information for each player here
 
     # attack1a = random.randint(1000,9999)
     # attack1b = random.randint(1000,9999)
@@ -62,15 +100,20 @@ char2 = character(char2_type, char2_name)
     #     char2.current_health -= 1
 
 
-#Combat happens
-while char1.current_health > 0 and char2.current_health > 0:
-    print("Initiating Next Round of Combat!")
-    char1_attack = int(input("Character 1, which attack do you want to use? Enter p for physical or m for magic: "))
-    os.system('clear')
-    char2_attack = int(input("Character 1, which attack do you want to use? Enter p for physical or m for magic: "))
-    os.system('clear')
 
-    #Lines 82 - 96 (if statements) should be character functions, and output damage returned to arena
+
+
+
+
+
+
+
+
+
+
+
+
+     #Lines 82 - 96 (if statements) should be character functions, and output damage returned to arena
     # if char1_attack == "p":
     #     damage = char1.physical_attack
     # else:
@@ -84,14 +127,3 @@ while char1.current_health > 0 and char2.current_health > 0:
     #     damage = char2.magic_attack #This means code will default to magic if any other thing is written
 
     # char2_damagedealt = damage + char2_weapon_damage - char1.armor
-
-    char2.current_health -= char1.damage_dealt #damage done to character 2
-    char1.current_health -= char2.damage_dealt #damage done to character 1
-    print("Char 1 health remaining: ", char1.current_health)
-    print("Char 2 health remaining: ", char2.current_health)
-
-#End of Combat
-if char1.current_health <= 0:
-    print("Character 1 has been defeated. Congratulations, character 2!")
-if char2.current_health <= 0:
-    print("Character 2 has been defeated. Congratulations, character 1!")
